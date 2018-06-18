36f966ac055d6edc18b9a98a6e76303d97310dda
ecombette <elise.combette@gmail.com>
2018-06-18 13:18:22 -0400

    Minor fixes

--- vgm_classif_model.py
+++ vgm_classif_model.py
@@ -7,26 +7,25 @@ def main():
 	[train_x, train_y], [eval_x, eval_y] = dataset_processor.process_data(FLAGS.dataset_dir)
 	
 	# Feature columns describe how to use the input.
-    matrix_feature_column = tf.feature_column.numeric_column(key="Notes", shape=[100,4])
-
+	matrix_feature_column = tf.feature_column.numeric_column(key="Notes", shape=[100,3])
+	
 	# Build a DNN with 2 hidden layers and 10 nodes in each hidden layer.
-    classifier = tf.estimator.DNNClassifier(
-    feature_columns=matrix_feature_column,
-    # Two hidden layers of 10 nodes each.
-    hidden_units=[10, 10],
-    # The model must choose between 3 classes (Zelda, Mario, Golden_Sun).
-    n_classes=3)
+	classifier = tf.estimator.DNNClassifier(
+		feature_columns=[matrix_feature_column],
+		# Two hidden layers of 10 nodes each.
+		hidden_units=[10, 10],
+		# The model must choose between 3 classes (Zelda, Mario, Golden_Sun).
+		n_classes=3)
 	
 	# Train the Model.
-    classifier.train(
-        input_fn=lambda:train_input_fn(train_x, train_y, FLAGS.batch_size),
-        steps=FLAGS.train_steps)
-
-    # Evaluate the model.
-    eval_result = classifier.evaluate(
-        input_fn=lambda:eval_input_fn(test_x, test_y, FLAGS.batch_size))
-
-    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+	classifier.train(
+		input_fn=lambda:train_input_fn(train_x, train_y, FLAGS.batch_size),
+		steps=FLAGS.train_steps)
+	
+	# Evaluate the model.
+	eval_result = classifier.evaluate(input_fn=lambda:eval_input_fn(eval_x, eval_y, FLAGS.batch_size))
+	
+	print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
 	print("TO DO : Generate predictions from the model")
 	
     
@@ -34,29 +33,29 @@ def main():
 def train_input_fn(features, labels, batch_size):
     """An input function for training"""
     # Convert the inputs to a Dataset.
-    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+    dataset = tf.data.Dataset.from_tensor_slices((features, labels))
 
     # Shuffle, repeat, and batch the examples.
     return dataset.shuffle(1000).repeat().batch(batch_size)
 
 def eval_input_fn(features, labels, batch_size):
 	"""An input function for evaluation or prediction"""
-    features = dict(features)
-    if labels is None:
-        # No labels, use only features.
-        inputs = features
-    else:
-        inputs = (features, labels)
+	features = dict(features)
+	if labels is None:
+		# No labels, use only features.
+		inputs = features
+	else:
+		inputs = (features, labels)
 	
 	# Convert the inputs to a Dataset.
-    dataset = tf.data.Dataset.from_tensor_slices(inputs)
-
-    # Batch the examples
-    assert batch_size is not None, "batch_size must not be None"
-    dataset = dataset.batch(batch_size)
-
-    # Return the dataset.
-    return dataset
+	dataset = tf.data.Dataset.from_tensor_slices(inputs)
+	
+	# Batch the examples
+	assert batch_size is not None, "batch_size must not be None"
+	dataset = dataset.batch(batch_size)
+	
+	# Return the dataset.
+	return dataset
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
@@ -73,7 +72,7 @@ if __name__ == "__main__":
 		help="Batch size")
 	parser.add_argument(
 		"--train_steps",
-		default=1000,
+		default=10000,
 		type=int,
 		help="Number of training steps")
 	FLAGS, _ = parser.parse_known_args()
