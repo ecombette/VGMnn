95a505a977c7992cb8bb7f3393d5c365e0e0d7d9
ecombette <elise.combette@gmail.com>
2018-06-18 00:07:02 -0400

    Add dataset processing and model functions
    The process_dataset() method still returns void lists, the randomized dataset must be split into the training and evaluation sets.
    The model still does not include predictions, only training and evaluation.

--- dataset_processor.py
+++ dataset_processor.py
@@ -1,14 +1,19 @@
 import os
+import random
 import pretty_midi
+import numpy as np
 
-def analyse_dataset(dataset_dir, classes):
+GAME_SERIES = ['Golden_Sun', 'Mario', 'Zelda']
+
+def analyse_dataset(dataset_dir):
+# Utility function for analyzing our dataset
 	min = 1000
 	max = 0
 	smallsequences = []
 	mean_size = 0
 	num_sequences = 0
 	
-	for game in classes:
+	for game in GAME_SERIES:
 		trackfile = dataset_dir + '/' + game + '_tracks.txt'
 		with open(trackfile) as fp:
 			line = fp.readline()
@@ -20,7 +25,7 @@ def analyse_dataset(dataset_dir, classes):
 				track = sequence.instruments[int(file_info[1])]
 				sequence_size = len(track.notes)
 				mean_size += sequence_size
-				if(sequence_size < 150): smallsequences.append([file_info[0], sequence_size])
+				if(sequence_size < 100): smallsequences.append([file_info[0], sequence_size])
 				if(sequence_size < min): min = sequence_size
 				if(sequence_size > max): max = sequence_size
 				line = fp.readline()
@@ -32,5 +37,38 @@ def analyse_dataset(dataset_dir, classes):
 	print(smallsequences)
 	print('Average sequence size : ' + str(mean_size / num_sequences))
 
-def process_dataset(dataset_dir, classes):
-	print('TO DO: process dataset')
+def process_dataset(dataset_dir):
+	dataset = []
+	
+	# Browse the MIDI files for each class
+	for game in GAME_SERIES:
+		trackfile = dataset_dir + '/' + game + '_tracks.txt'
+		with open(trackfile) as fp:
+			line = fp.readline()
+			while line:
+				file_info = line.split()
+				print(file_info) # LOG
+				# Retrieve the melody track from the MIDI file
+				sequence = pretty_midi.PrettyMIDI(dataset_dir + '/' + game + '/' + file_info[0] + '.mid')
+				track_matrix = np.zeros((100,4))
+				track = sequence.instruments[int(file_info[1])]
+				
+				# Fill the notes in the matrix (no more than 100)
+				track_size = len(track.notes) if len(track.notes) <=100 else 100
+				for i in range(track_size):
+					track_matrix[i] = track[i]
+				
+				# Add entry to dataset container
+				dataset.append([track_matrix, game])
+				line = fp.readline()
+	
+	# Randomize the dataset
+	random.shuffle(dataset)
+	
+	# TO DO :
+	# Fill the training set with the first 25 samples
+	# Fill the evaluation set with the next 20 samples
+	# Keep track of unused MIDI files for predictions?
+	
+	return [train_x, train_y], [eval_x, eval_y]
+
--- vgm_classif_model.py
+++ vgm_classif_model.py
@@ -3,7 +3,60 @@ import tensorflow as tf
 import dataset_processor
 
 def main():
-	dataset_processor.analyse_dataset(FLAGS.dataset_dir, ['Golden_Sun', 'Mario', 'Zelda'])
+	# Retrieve training and evaluation data
+	[train_x, train_y], [eval_x, eval_y] = dataset_processor.process_data(FLAGS.dataset_dir)
+	
+	# Feature columns describe how to use the input.
+    matrix_feature_column = tf.feature_column.numeric_column(key="Notes", shape=[100,4])
+
+	# Build a DNN with 2 hidden layers and 10 nodes in each hidden layer.
+    classifier = tf.estimator.DNNClassifier(
+    feature_columns=matrix_feature_column,
+    # Two hidden layers of 10 nodes each.
+    hidden_units=[10, 10],
+    # The model must choose between 3 classes (Zelda, Mario, Golden_Sun).
+    n_classes=3)
+	
+	# Train the Model.
+    classifier.train(
+        input_fn=lambda:train_input_fn(train_x, train_y, FLAGS.batch_size),
+        steps=FLAGS.train_steps)
+
+    # Evaluate the model.
+    eval_result = classifier.evaluate(
+        input_fn=lambda:eval_input_fn(test_x, test_y, FLAGS.batch_size))
+
+    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+	print("TO DO : Generate predictions from the model")
+	
+    
+
+def train_input_fn(features, labels, batch_size):
+    """An input function for training"""
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle, repeat, and batch the examples.
+    return dataset.shuffle(1000).repeat().batch(batch_size)
+
+def eval_input_fn(features, labels, batch_size):
+	"""An input function for evaluation or prediction"""
+    features = dict(features)
+    if labels is None:
+        # No labels, use only features.
+        inputs = features
+    else:
+        inputs = (features, labels)
+	
+	# Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices(inputs)
+
+    # Batch the examples
+    assert batch_size is not None, "batch_size must not be None"
+    dataset = dataset.batch(batch_size)
+
+    # Return the dataset.
+    return dataset
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
@@ -13,5 +66,16 @@ if __name__ == "__main__":
 		type=str,
 		default="",
 		help="Path to training data")
+	parser.add_argument(
+		"--batch_size",
+		default=50,
+		type=int,
+		help="Batch size")
+	parser.add_argument(
+		"--train_steps",
+		default=1000,
+		type=int,
+		help="Number of training steps")
 	FLAGS, _ = parser.parse_known_args()
 	main()
+
